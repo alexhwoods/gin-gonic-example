c07e5de77e07cafed62942a24e7e40ff82b65096
Alexander Woods <alex.w@ware2go.co>
2020-05-18 14:14:58 -0400

    Add a simple endpoint

---
+++ hello.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	http.ListenAndServe(":3000", r)
+}
