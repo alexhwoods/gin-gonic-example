5f937cb0b2783189749a44bedc7ca4b74ff1ed4d
Alexander Woods <alex.w@ware2go.co>
2020-05-18 14:29:32 -0400

    Add some endpoints

--- hello.go
+++ hello.go
@@ -1,16 +1,36 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
+type person struct {
+	name string
+	age  int
+}
+
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
 
-	r.Run(":3000")
+	router.GET("/hello/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.String(http.StatusOK, "Hello %s", name)
+	})
+
+	router.POST("/hello/:name", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"status":  "posted",
+			"message": "adf",
+			"nick":    "adadf",
+		})
+	})
+
+	router.Run(":3000")
 }
